85a4d214c4858138e2e494c66891c91fcc951e6c
Jakub Horosz <kubahorosz95@gmail.com>
2018-12-03 23:32:41 +0100

    Implemented recipe

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,48 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end 
+
+  def show
+    @recipes = Recipe.find(params[:id])
+  end 
+
+  def new
+    @racipes = Recipe.new
+  end 
+
+  def create
+    @recipes = Recipe.create(recipe_params)
+    if @recipes.persisted?
+      redirect_to root_path
+    else
+      render :new
+    end
+  end 
+    
+  def edit
+    @recipe = Recipe.new(params[:id])
+    render :new
+  end 
+
+  def destroy
+    Recipe.destroy(params[:id])
+    redirect_to recipe_path
+  end 
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path
+    else
+      render :new
+    end
+  end 
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:id, :coffee, :quantity, :method, :water_temperature)
+  end 
+  
+end 
---
+++ db/migrate/20181128140034_create_recipe.rb
@@ -0,0 +1,22 @@
+class CreateRecipe < ActiveRecord::Migration[5.1]
+  def change
+    create_table :recipes do |t|
+    t.string  :coffee
+    t.decimal :quantity,          :precision => 4, scale: 1
+    t.string  :method
+    t.integer :water_temperature, :precision => 4, scale: 1
+    t.decimal :water_amount,      :precision => 4, scale: 1
+    t.string  :grind
+    t.string  :aroma
+    t.integer :aroma_points
+    t.string  :taste
+    t.integer :taste_points
+    t.string  :body
+    t.integer :body_points
+    t.string  :astringency
+    t.integer :astringency_points 
+  
+    t.timestamps
+    end
+  end
+end
