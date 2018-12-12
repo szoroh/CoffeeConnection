76dc538bc98c898821483e2ae9e28098895c7ff0
Jakub Horosz <kubahorosz95@gmail.com>
2018-12-12 16:24:08 +0100

    created models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,26 @@
-class Recipe < ApplicationRecord  
+class Recipe < ApplicationRecord 
+  belongs_to :coffee_profile
+
+  validates :coffee, presence: true,
+                     length: { in: 3..70 }
+  validates :quantity, presence: true, 
+                       numericality: { greater_than_or_equal_to: 0 }
+  validates :method, presence: true 
+  validates :water_temperature, presence: true,
+                                numericality: { in: 0..100 }
+  validates :water_amount, presence: true,
+                           numericality: { in: 0..500 }
+  validates :girnd, presence: true 
+  validates :aroma, presence: true 
+  validates :aroma_points, presence: true,
+                           numericality: { in: 0..25 }
+  validates :taste, presence: true
+  validates :taste_points, presence: true,
+                           numericality: { in: 0..25 }
+  validates :body, presence: true 
+  validates :body_points, presence: true, 
+                          numericality: { in: 0..25 }
+  validates :astringency, presence: true
+  validates :astringency_points, presence: true,
+                                 numericality: { in: 0..25 }
 end 
