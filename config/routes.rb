3aa70ed88467e0de8c4f8e51577879349fcdb8b3
Jakub Horosz <kubahorosz95@gmail.com>
2019-01-25 10:44:33 +0100

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   authenticated do
-    root to: 'user#index'
+    root to: 'users#index'
   end 
 
   root to: 'home#index'
-
-  devise_for :users
   
   resources :recipes
   resources :coffee_profiles
+
+  devise_for :users
 end
