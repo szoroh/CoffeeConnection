d0bc4a296750a801d3bd0dab3c541368f42e3004
Jakub Horosz <kubahorosz95@gmail.com>
2019-01-25 10:40:14 +0100

    Bump user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  #has_many :recipes
+  has_many :recipes, dependent: :destroy
+  has_many :coffee_profiles, dependent: :destroy
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
   
-  #validates :name, presence: true, length: { in: 3..30 }
+  validates :name, presence: true, length: {maximum: 50}
 end
