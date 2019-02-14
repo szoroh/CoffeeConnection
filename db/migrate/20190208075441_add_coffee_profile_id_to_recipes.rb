3a7e5757112656aa2999b48894135bc318d154b0
Jakub Horosz <kubahorosz95@gmail.com>
2019-02-14 16:42:13 +0100

    Add foreign key to recipes column

--- db/migrate/20190208075441_add_coffee_profile_id_to_recipes.rb
+++ db/migrate/20190208075441_add_coffee_profile_id_to_recipes.rb
@@ -1,5 +1,5 @@
 class AddCoffeeProfileIdToRecipes < ActiveRecord::Migration[5.1]
   def change
-    add_reference :recipes, :coffee_profile
+    add_reference :recipes, :coffee_profile, foreign_key: true
   end
 end
